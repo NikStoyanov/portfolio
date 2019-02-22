dfa2386661a181ed5988b44fda0739be30d1af6d
Nikola Stoyanov <nstoyanov.92@gmail.com>
2019-02-22 14:10:53 +0000

    Increased default execute time for notebook

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 read -p "Folder path: " npath
 read -p "Notebook name: " notebook
-jupyter nbconvert $npath/$notebook.ipynb --execute --allow-errors --output-dir $npath --to markdown --template temp.tpl --NbConvertApp.output_files_dir=.
+jupyter nbconvert $npath/$notebook.ipynb --execute --allow-errors --output-dir $npath --to markdown --template temp.tpl --ExecutePreprocessor.timeout=500 --NbConvertApp.output_files_dir=.
 cat $npath/$notebook.md | tee -a $npath/index.mmark
 rm $npath/$notebook.md
