bf7039f004946f258457573d11ec00897918a512
Nikola Stoyanov <nik@nikstoyanov.me>
2025-03-01 14:57:04 +0000

    Website bankruptcy

---
+++ content/_index.md
@@ -0,0 +1,8 @@
++++
+title = "Nikola Stoyanov"
++++
+
+# About
+I’m an engineer at Arm working in the Data Engineering Platform team. This blog is where I write about things I find fun and personally interesting. Opinions are my own and I do not speak for my employer.
+
+---
--- export.sh
+++
@@ -1,5 +0,0 @@
-read -p "Folder path: " npath
-read -p "Notebook name: " notebook
-jupyter nbconvert $npath/$notebook.ipynb --execute --allow-errors --output-dir $npath --to markdown --template temp.tpl --ExecutePreprocessor.timeout=500 --NbConvertApp.output_files_dir=.
-cat $npath/$notebook.md | tee -a $npath/index.mmark
-rm $npath/$notebook.md
